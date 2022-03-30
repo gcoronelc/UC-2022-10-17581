804e423e46bbaac82c5737a6038692d5e3c35ed2
Gustavo Coronel <gcoronelc@gmail.com>
2022-03-30 10:05:02 -0500

    Semana 2

---
+++ Semana02/Problema03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	
+	
+	// Lectura de datos
+	
+	
+	// Proceso
+	
+	
+	// Reporte
+	
+	
+	// Fin
+	system("pause");
+	return 0;
+}
+
