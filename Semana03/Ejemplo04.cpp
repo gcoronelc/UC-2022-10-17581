3beae25cac074ce195b10fe91abbd63d2ffae8a1
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-06 08:15:09 -0500

    Semana 3

---
+++ Semana03/Ejemplo04.cpp
@@ -0,0 +1,56 @@
+/*
+Estructura: switch
+
+
+	switch(expresion){
+		case valor1:
+			instrucciones;
+			[break;]
+		case valor2:
+			instrucciones;
+			[break;]
+		case valor3:
+			instrucciones;
+			[break;]
+			...
+		case valorn:
+			instrucciones;
+			[break;]
+		[default:
+			instrucciones;
+			[break;]			
+		]					
+	}
+
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	int n = 3;
+	string dato;
+	
+	switch(n){
+		case 1:
+			dato = "Hola todos.";
+			break;
+		case 2:
+			dato = "Adios todos.";
+			break;
+		default:
+			dato = "Opción incorrecta.";
+			break;
+	}
+	
+	cout << "Dato: " << dato << endl;
+	cout << endl;
+	
+	// Fin
+	system("pause");
+	return 0;
+}
+
