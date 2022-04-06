4bd3f52d13ff6de889714eccc441e1be642f8ab1
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-06 10:41:43 -0500

    Semana 3

--- Semana03/PROBLEMA02/Problema2_V2.cpp
+++ Semana03/PROBLEMA02/Problema2_V2.cpp
@@ -17,9 +17,13 @@ int main(){
 	// Proceso
 	// Paso 1: Determinar % de descuento
 	porcDcto = 0;
-	porcDcto = (sueldo>=0 && sueldo<=1000.0)?10:porcDcto;
-	porcDcto = (sueldo>1000.0 && sueldo<=2000.0)?15:porcDcto;
-	porcDcto = (sueldo>2000.0)?20:porcDcto;
+	if(sueldo>=0 && sueldo<=1000.0){
+		porcDcto = 10;
+	} else if(sueldo>1000.0 && sueldo<=2000.0){
+		porcDcto = 15;
+	} else if(sueldo>2000.0){
+		porcDcto = 20;
+	}
 	// Paso 2: Calcular el descuento
 	descuento = sueldo * porcDcto / 100;
 	// Paso 3: Calcular el sueldo neto
