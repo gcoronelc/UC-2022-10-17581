9ebab9ccc58b59647b4ef900530c98527c852dfd
Gustavo Coronel <gcoronelc@gmail.com>
2022-04-27 10:28:55 -0500

    Semana 6

--- Semana06/Vendedor.cpp
+++ Semana06/Vendedor.cpp
@@ -8,6 +8,8 @@ int main(){
 	
 	// Variables
 	int opMenu;
+	double ventas, meta, porcComision, porcBono;
+	double comision, bono, pagoTotal;
 	
 	// Control del menú de opciones
 	do{
@@ -39,30 +41,48 @@ int main(){
 			return 0;
 		}
 		
+		// Lectura de datos
+		cout << endl;
+		cout << "Lectura de datos" << endl;
+		cout << "================================" << endl;
+		cout << "Ventas: "; cin >> ventas;
+		
 		// Procesar opción del menú
-		system("cls");
 		switch(opMenu){
-			// Opción 1: aaaa
 			case 1:
-				cout << "Proceso opción 1" << endl;
+				meta = 80000.0;
+				porcComision = 0.10;
+				porcBono = 0.03;
 				break;
-			// Opción 2: bbbbb
 			case 2:
-				cout << "Proceso opción 2" << endl;
+				meta = 50000.0;
+				porcComision = 0.12;
+				porcBono = 0.05;
 				break;	
-			// Opción 3: cccccccc
 			case 3:
-				cout << "Proceso opción 3" << endl;
+				meta = 70000.0;
+				porcComision = 0.15;
+				porcBono = 0.07;
 				break;					
-			// Opción 4: dddddddd
 			case 4:
-				cout << "Proceso opción 4" << endl;
-				break;
-			// Opción 5: Salir
-			case 5:
-				cout << "Fin del programa." << endl;
+				meta = 100000.0;
+				porcComision = 0.16;
+				porcBono = 0.08;
 				break;
 		}
+		comision = (ventas>meta?meta:ventas) * porcComision;
+		bono = (ventas>meta?ventas-meta:0) * porcBono;
+		pagoTotal = comision + bono;
+		
+		// Reporte
+		cout << endl;
+		cout << "Reporte" << endl;
+		cout << "------------------------------------" << endl;
+		cout << "Ventas: " << ventas << endl;
+		cout << "Comisión: " << comision << endl;
+		cout << "Bono: " << bono << endl;
+		cout << "Pago total: " << pagoTotal << endl;
+		cout << endl;
 		
 		system("pause");
 	} while(opMenu<5);
