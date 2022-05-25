0cfc5de00ee08cb6501bc7edf60251b9c303d32d
Gustavo Coronel <gcoronelc@gmail.com>
2022-05-25 08:35:02 -0500

    Semana 10

---
+++ Semana10/Magia01.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int LeeEntero(string etiqueta){
+	int valor;
+	cout << etiqueta;
+	cin >> valor;
+	return valor;
+}
+
+int LeeEntero(string etiqueta, int valorMinimo){
+	int valor;
+	do{
+		cout << etiqueta; cin >> valor;
+		if(valor<valorMinimo){
+			cout << "ERROR: dato ingresado fuera de rango." << endl;
+		}
+	}while(valor<valorMinimo);
+	return valor;
+}
+
+int LeeEntero(string etiqueta, int valorMinimo, int valorMaximo){
+	int valor;
+	do{
+		cout << etiqueta; cin >> valor;
+		if(valor<valorMinimo || valor>valorMaximo){
+			cout << "ERROR: dato ingresado fuera de rango." << endl;
+		}
+	}while(valor<valorMinimo || valor>valorMaximo);
+	return valor;
+}
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	int edad, cantidad, peso;
+  
+	// Lectura de datos
+	cout << "LA MAGIA EXISTE" << endl;
+	edad = LeeEntero("Ingrese la edad [1,120]: ",1,120);
+	cantidad = LeeEntero("Ingrese la cantidad (>0): ",1);
+	peso = LeeEntero("Ingrese el peso (Kg) (>=0): ",0);
+	
+	// Reporte
+	cout << endl;
+	cout << "REPORTE" << endl;
+	cout << "Edad: " << edad << endl;
+	cout << "Cantidad: " << cantidad << endl;
+	cout << "Peso: " << peso << endl;
+	cout << endl;
+	
+	// Fin
+	system("pause");
+	return 0;
+}
