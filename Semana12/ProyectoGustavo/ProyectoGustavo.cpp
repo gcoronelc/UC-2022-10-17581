9657e73cd3a20f191dea077f410e1504ac97fad3
Gustavo Coronel <gcoronelc@gmail.com>
2022-06-08 08:53:48 -0500

    Semana 12

--- Semana12/ProyectoGustavo/ProyectoGustavo.cpp
+++ Semana12/ProyectoGustavo/ProyectoGustavo.cpp
@@ -5,25 +5,35 @@ using namespace std;
 
 // Funciones y procedimientos
 void PresentarMenu();
-
+void ProcesarVenta();
+void ReporteEstadistico();
+void FinPrograma();
 
 int main(){
-	
 	// Configuracion
 	setlocale(LC_CTYPE,"Spanish");
-	
 	// Variables
 	int opMenu;
-	
 	// Proceso principal
 	do{
+		// Menu
 		PresentarMenu();
 		opMenu = LeerOpcion(1,3);
-		
-		cout << "Opcion: " << opMenu << endl;
-		system("pause");
+		// Procesar el menú
+		system("cls");
+		switch(opMenu){
+			case 1:
+				ProcesarVenta(); break;
+			case 2:
+				ReporteEstadistico(); break;
+			case 3:
+				FinPrograma(); break;
+		}
+		// Control de la pausa
+		if(opMenu!=3){
+			system("pause");
+		}
 	} while(opMenu!=3);
-	
 	// Fin
 	system("pause");
 	return 0;
@@ -39,4 +49,18 @@ void PresentarMenu(){
 	cout << "\t3.- Salir" << endl;
 	cout << "================================" << endl;
 	cout << endl;
+}
+
+void ProcesarVenta(){
+	cout << "Falta ..." << endl;
+}
+
+void ReporteEstadistico(){
+	cout << "Falta ..." << endl;
+}
+
+void FinPrograma(){
+	cout << "FIN DEL PROGRAMA" << endl;
+	cout << "Esperamos su pronta visita" << endl;
+	cout << "Que tenga un maravilloso día" << endl;
 }
