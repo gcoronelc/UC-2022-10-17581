3ddf5b3b94d7bf51a29219c861c27992ddf4a921
Gustavo Coronel <gcoronelc@gmail.com>
2022-07-06 08:19:17 -0500

    Semana 16

---
+++ Semana16/Grupo20/ProyectoGpo20.cpp
@@ -0,0 +1,51 @@
+/*
+**EQUIPO:
+**   - Gustavo Coronel
+**   - CHELSI CAROLINA HUARCAYA CENTENO
+**   - MANYORI IPARRAGUIRRE RAMOS
+**   - JUAN JOSE GONZALES DEL VALLE PEVES
+**   - EDUARDO ÑAUPA YAPURA
+**   - MIDWAR ERICK ESCOBAR QUISPE
+*/
+
+#include <iostream>
+#include "ToolBox.h"
+#include "utilitario.h"
+using namespace std;
+
+int main(){
+	
+	// Variables del programa
+	int op;
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Proceso principal
+	do{
+		// El menú
+		mostrarMenu();
+		op = LeerOpcion(1,3);
+		// Procesar opcion del menú
+		system("cls");
+		switch(op){
+			case 1:
+				procesarVenta();
+				break;
+			case 2:
+				reporteVentas();
+				break;
+			case 3:
+				finPrograma();
+				break;
+		}
+		
+		// La pausa
+		if(op!=3){
+			system("pause");
+		}
+	} while(op!=3);
+	
+	// Fin
+	return 0;
+}
---
+++ Semana16/Grupo20/utilitario.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void mostrarMenu(){
+	system("cls");
+	cout << "EL PESCADOR" << endl;
+	cout << "===============================" << endl;
+	cout << "1.- Registrar venta" << endl;
+	cout << "2.- Reporte de ventas" << endl;
+	cout << "3.- Salir" << endl;
+	cout << "===============================" << endl;
+}
+
+void procesarVenta(){
+	cout << "Falta poco" << endl;
+}
+
+void reporteVentas(){
+	cout << "Ventas ok" << endl;
+}
+
+void finPrograma(){
+	cout << "Fin del programa" << endl;
+	cout << "Que tanga un excelente día" << endl;
+	cout << endl;
+}
